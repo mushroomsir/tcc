8fd306ed455c0fd46b7e5c18fd9054c9ee27b4c5
mushroomsir <mushroomsir@163.com>
2020-06-09 19:57:42 +0800

    update field name

--- lock/mysql/lock.go
+++ lock/mysql/lock.go
@@ -15,33 +15,33 @@ type Lock struct {
 }
 
 // Lock ...
-func (a *Lock) Lock(key string, expire time.Duration) error {
+func (a *Lock) Lock(name string, expire time.Duration) error {
 	lock := &dto.Lock{}
 
 	now := time.Now().UTC().Add(expire)
-	insertSql := "insert into `tcc_lock` (`key`,`expire_at`) values (?, ?)"
-	err := a.DB.Exec(insertSql, key, now).Error
+	insertSql := "insert into `tcc_lock` (`name`,`expire_at`) values (?, ?)"
+	err := a.DB.Exec(insertSql, name, now).Error
 	if err != nil {
-		findSql := "select * from tcc_lock where `key` = ? limit 1"
-		e := a.DB.Raw(findSql, key).Scan(lock).Error
+		findSql := "select * from tcc_lock where `name` = ? limit 1"
+		e := a.DB.Raw(findSql, name).Scan(lock).Error
 		if e == nil {
 			if lock.ExpireAt.Before(now) {
-				a.Unlock(key) // release lock
-				err = a.DB.Exec(insertSql, key, now).Error
+				a.Unlock(name) // release lock
+				err = a.DB.Exec(insertSql, name, now).Error
 			}
 		}
 	}
 	if err != nil {
-		err = fmt.Errorf("%s locked, should expire at: %v, error: %s", key, expire, err.Error())
+		err = fmt.Errorf("%s locked, should expire at: %v, error: %s", name, expire, err.Error())
 	}
 	return err
 }
 
 // Unlock ...
-func (a *Lock) Unlock(key string) {
-	sql := "delete from tcc_lock where `key` = ?"
-	err := a.DB.Exec(sql, key).Error
+func (a *Lock) Unlock(name string) {
+	sql := "delete from tcc_lock where `name` = ?"
+	err := a.DB.Exec(sql, name).Error
 	if err != nil {
-		alog.Errf("unlock: key %s, error %v", key, err.Error())
+		alog.Errf("unlock: key %s, error %v", name, err.Error())
 	}
 }
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `test_tcc` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
 CREATE TABLE IF NOT EXISTS `test_tcc`.`tcc_async_task` (
     `id` bigint NOT NULL AUTO_INCREMENT,
     `created_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -10,10 +11,11 @@ CREATE TABLE IF NOT EXISTS `test_tcc`.`tcc_async_task` (
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_uid` (`uid`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
+
 CREATE TABLE IF NOT EXISTS `test_tcc`.`tcc_lock` (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    `key` varchar(127) NOT NULL,
+    `name` varchar(127) NOT NULL,
     `expire_at` datetime(3) NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_key` (`key`)
+    UNIQUE KEY `uk_name` (`name`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
